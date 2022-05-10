2efe72a5ebd4950d06534671ac04a51bd99b38a0
taidang1 <tdang4498@gmail.com>
2022-05-09 20:57:49 -0400

    Update app.js

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose')
-const Blog = require('./models/blog');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
